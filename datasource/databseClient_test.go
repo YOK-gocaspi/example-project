a14f7e6ac60a8d5ffcc40905d512e425475e61d5
fee <f.stein@gocaspi.de>
2022-08-12 15:14:51 +0200

    Refactor dbclient_test.go

--- datasource/databseClient_test.go
+++ datasource/databseClient_test.go
@@ -1,12 +1,10 @@
 package datasource_test
 
 import (
-	"context"
 	"example-project/datasource"
 	"example-project/datasource/datasourcefakes"
 	"fmt"
 	"github.com/stretchr/testify/assert"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"testing"
 )
@@ -20,24 +18,4 @@ func TestUpdateMany(t *testing.T) {
 	actual := dbClient.UpdateMany([]interface{}{})
 	fmt.Println(actual)
 	assert.NotEmpty(t, actual)
-}
-
-func TestGetAllEmployees(t *testing.T) {
-	fakeDB := &datasourcefakes.FakeMongoDBInterface{}
-	// fakeDB.InsertManyReturns(&mongo.InsertManyResult{InsertedIDs: []interface{}{"1"}}, nil)
-	cursor, _ := fakeDB.Find(context.TODO(), bson.M{})
-	fakeDB.FindReturns(cursor, nil)
-	dbClient := datasource.Client{
-		Employee: fakeDB,
-	}
-	result := dbClient.GetAll()
-	fmt.Println("Get All:", result)
-	/*
-		// DB erstellen -> Daten schreiben -> Input == Output
-
-		dbClient := datasource.Client{
-			Employee: fakeDB,
-		}
-		actual := dbClient.GetAll()
-	*/
 }
